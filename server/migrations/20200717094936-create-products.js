5a0d09d25f6d76a5d1b26f3220e4b4e43ac259ce
dumadoniagara <dumadsambora@gmail.com>
2020-07-17 16:56:53 +0700

    migrate models

---
+++ server/migrations/20200717094936-create-products.js
@@ -0,0 +1,63 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      colors: {
+        type: Sequelize.JSON
+      },
+      rate: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      brand: {
+        type: Sequelize.STRING
+      },
+      detail_product: {
+        type: Sequelize.TEXT
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      testimonials: {
+        type: Sequelize.JSON
+      },
+      vote: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      capacities: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ server/models/products.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Products.init({
+    title: DataTypes.STRING,
+    category: DataTypes.STRING,
+    colors: DataTypes.JSON,
+    rate: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    brand: DataTypes.STRING,
+    detail_product: DataTypes.TEXT,
+    image: DataTypes.STRING,
+    testimonials: DataTypes.JSON,
+    vote: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    stock: DataTypes.INTEGER,
+    capacities: DataTypes.JSON
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Products;
+};
